0d84493b056deb0d4f97f2fee46cd213c2ad4852
Wouter Groeneveld <jefklak@gmail.com>
2011-06-29 10:52:22 +0200

    modified jquery include

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -2,7 +2,7 @@ load("./../src/test/javascript/lib/env.rhino.1.2.js");
 load("./../src/test/javascript/lib/env.utils.js");
 load("./../src/test/javascript/envJsOptions.js");
 
-load("jquery-1.6.1.js");
+load("./../src/test/javascript/jquery-1.6.1.js");
 
 function append(itm) {
 	$('body').append($(itm));
